c006bdb69ee899865d49eaafc809b3072b2e4765
Apisathan Sivananthanathan <apisathan2000@gmail.com>
2025-05-31 12:08:16 +0530

    port change

--- user-service/src/index.js
+++ user-service/src/index.js
@@ -1,7 +1,7 @@
 import app from './app.js';
 
-const PORT = process.env.PORT || 5001;
-// const PORT = 5001;
+// const PORT = process.env.PORT || 5001;
+const PORT = 5001;
 app.listen(PORT, () => {
   console.log(`User service running on port ${PORT}`);
 });
