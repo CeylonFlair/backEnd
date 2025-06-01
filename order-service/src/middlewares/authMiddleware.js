621004f91542f2efd75ba8a2dcc1c76dc9bd0d6a
Apisathan Sivananthanathan <apisathan2000@gmail.com>
2025-06-01 19:45:30 +0530

    internal service tokens added

--- order-service/src/middlewares/authMiddleware.js
+++ order-service/src/middlewares/authMiddleware.js
@@ -4,6 +4,12 @@ export const protect = (req, res, next) => {
   let token = req.headers.authorization?.split(' ')[1];
   if (!token) return res.status(401).json({ message: 'No token. Authorization denied.' });
 
+
+  if (token === process.env.ORDER_SERVICE_TOKEN) {
+    req.user = { service: true }; // Mark as internal service
+    return next();
+  }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
